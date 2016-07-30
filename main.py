5b793c16b89fde6eab9ebfb3cba153c51b2483a7
kcrong <kcrong@kim82536.pe.kr>
2016-07-31 02:34:52 +0900

    객체지향으로 리펙토링. 아호 코라식만 추가하면 됨

--- main.py
+++ main.py
@@ -3,40 +3,52 @@ from pydivert.windivert import *
 with open('mal_site_edited.txt', 'r') as f:
     bad_urls = [url.split('\n')[0] for url in f.readlines()]
 
-
-def find_host(payload):
-    payload = payload.decode('utf-8')
-    try:
-        head_idx = payload.index('Host: ') + len('Host: ')
-        end_idx = payload.index('\r\nConnection', head_idx)
-    except ValueError:
-        return False
-    else:
-        return payload[head_idx:end_idx]
-
-
 PROJECT_ROOT = os.path.dirname(os.path.abspath(sys.executable))
 WINDIVERT_DLL_PATH = os.path.join(PROJECT_ROOT, 'DLLs', "WinDivert.dll")
 
-# Init
-WinDivert(WINDIVERT_DLL_PATH).register()
 
-# Get Driver
-driver = WinDivert(WINDIVERT_DLL_PATH)  # WinDivertOpen
+class Filter:
+    def __init__(self):
+        self.driver = WinDivert(WINDIVERT_DLL_PATH)  # WinDivertOpen
+        self.handle = self.get_handle("outbound and tcp.DstPort == 80")
 
-# priority 는 기본적으로 1000을 많이 준다고 함.
-with Handle(driver, filter="outbound and tcp.DstPort == 80", priority=1000) as handle:
-    while True:
-        raw, meta = handle.recv()
-        captured_packet = driver.parse_packet(raw)
+    def get_handle(self, filter_text):
+        return Handle(self.driver, filter_text, priority=1000)
 
-        # If here is HTTP payload
-        if len(captured_packet.payload) != 0:
-            host = find_host(captured_packet.payload)
-            if host and host not in bad_urls:
-                print("Hello~ %s" % host)
-                handle.send(raw, meta)
+    @staticmethod
+    def find_host(payload):
+        payload = payload.decode('utf-8')
+        try:
+            head_idx = payload.index('Host: ') + len('Host: ')
+            end_idx = payload.index('\r\nConnection', head_idx)
+        except ValueError:
+            return False
+        else:
+            return payload[head_idx:end_idx]
+
+    def filter_host(self, host):
+        # We have to make Aho-Corasick!!!!
+        return False
+
+    def run(self):
+        while True:
+            raw, meta = self.handle.recv()
+            captured = self.driver.parse_packet(raw)
+
+            # if payload?
+            if len(captured.payload) != 0:
+                host = self.find_host(captured.payload)
+
+                if host and self.filter_host(host):
+                    print("Hello~ %s" % host)
+                    self.handle.send(raw, meta)
+                else:
+                    print("Bye~ %s" % host)
+
+            # Not Payload, Just go....
             else:
-                print("Bye~ %s" % host)
+                self.handle.send(raw, meta)
 
-        handle.send(raw, meta)
+if __name__ == '__main__':
+    f = Filter()
+    f.run()
