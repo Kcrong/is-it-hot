1a6f354257faae9d0ac55f31cc5a34a860203695
kcrong <kcrong@kim82536.pe.kr>
2016-07-31 10:38:05 +0900

    사용하지 않는 failure 함수 제거 각 함수 별 주석 추가

--- main.py
+++ main.py
@@ -15,6 +15,10 @@ class Filter:
 
     @staticmethod
     def find_host(payload):
+        """
+        :param payload: packet bytes - string
+        :return: host string ( www.google.com )
+        """
         payload = payload.decode('utf-8')
         try:
             head_idx = payload.index('Host: ') + len('Host: ')
@@ -25,10 +29,19 @@ class Filter:
             return payload[head_idx:end_idx]
 
     def filter_host(self, host):
+        """
+        Using Aho-Corasick, check is it bad url
+        :param host: host string ( www.google.com )
+        :return: boolean
+        """
         # Do Aho-Corasick!!!!
         return self.filter.search_pattern(host)
 
     def run(self):
+        """
+        드라이버를 열어 패킷을 가져옵니다.
+        :return: None ( Demon )
+        """
         print("Running...")
         print("Start to filtering")
         with Handle(self.driver, "outbound and tcp.DstPort == 80", priority=1000) as handle:
@@ -66,6 +79,10 @@ class AhoCorasickNode:
 
     @classmethod
     def root_node(cls):
+        """
+        루트 노드를 반환
+        :return: class root_node
+        """
         return cls._root_node
 
     @classmethod
@@ -82,18 +99,29 @@ class AhoCorasickNode:
         return "<AhoCorasickNode %s>" % self.char
 
     def set_next_node(self, next_node):
+        """
+        노드의 다음 노드 설정
+        :param next_node: next_node 로 설정될 노드
+        :return: None
+        """
         self._next_node[next_node.char] = next_node
         next_node.set_before_node(self)
 
     def set_before_node(self, before_node):
+        """
+        :param before_node: before_node 로 설정할 노드
+        :return: None
+        """
         self._before_node = before_node
         self._depth = self._before_node.depth + 1
 
-    @classmethod
-    def failure(cls):
-        return cls.root_node
-
     def next_node(self, char=None):
+        """
+        노드의 다음 경로에서 인자로 받은 char 의 노드를 반환합니다.
+        char를 특별히 지정하지 않았을 경우, 모든 다음 노드를 반환합니다.
+        :param char: 찾을 char
+        :return:  next_node or all_next_node_dictionary
+        """
         if char is None:
             return self._next_node
         try:
@@ -117,27 +145,47 @@ class AhoCorasick:
         self.root_node = self.node_ptr = AhoCorasickNode('root')
 
     def add_pattern(self, word):
+        """
+        word_tree 에서 연산할 단어를 추가
+        :param word: word to add
+        :return: None
+        """
         self.word_list.append(word)
 
     @staticmethod
     def _new_link(node, char, output_link=False):
+        """
+        :param node: 다음 경로를 설정할 노드
+        :param char: 다음 경로로 설정될 노드의 char 값
+        :param output_link: output_link 여부
+        :return: None
+        """
         node.set_next_node(AhoCorasickNode(char, output_link))
 
     def make_tree(self):
+        """
+        self 의 word_list 를 이용해 단어 tree 를 만듬.
+        :return: None
+        """
         for word in self.word_list:
             self.node_ptr = self.root_node
             for char in word[:-1]:
-                if self.node_ptr.next_node(char) is False:  # char가 해당 경로에 없을 경우
-                    self._new_link(self.node_ptr, char)
+                if self.node_ptr.next_node(char) is False:  # char 가 해당 경로에 없을 경우
+                    self._new_link(self.node_ptr, char)  # 새로운 다음 경로의 노드를 만듬
 
-                self.node_ptr = self.node_ptr.next_node(char)
+                self.node_ptr = self.node_ptr.next_node(char)  # 다음 노드로 포인터를 옮김
 
-            if self.node_ptr.next_node(word[-1]) is False:
-                self._new_link(self.node_ptr, word[-1], output_link=True)
+            if self.node_ptr.next_node(word[-1]) is False:  # word 의 마지막 글자 부분에서 다음 경로가 없을 경우
+                self._new_link(self.node_ptr, word[-1], output_link=True)  # output_link 를 활성화한 노드를 생성
             else:
-                self.node_ptr.output_link = True
+                self.node_ptr.output_link = True  # 있으면 해당 노드의 output_link 를 활성화
 
     def pretty_print(self, node=None):
+        """
+        생성된 노드를 출력합니다.
+        :param node: 부모 노드. (없을 경우 root_node 부터 출력 --> 모두 출력)
+        :return: None. Just recursion
+        """
         if node is None:
             node = self.root_node
 
@@ -157,6 +205,11 @@ class AhoCorasick:
                 self.pretty_print(next_node_dict[key])
 
     def search_pattern(self, pattern):
+        """
+        생성된 tree 를 이용해 인자로 받은 pattern 에서 substring 을 찾음
+        :param pattern: 찾을 문자열
+        :return: boolean
+        """
         node_ptr = self.root_node
 
         for char in pattern:
